6b74c4ad934e1baa49013a18ce049efd6fce93c0
estrella272729 <estrella272729@gmail.com>
2025-10-22 02:21:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.caption(f"Versión de Python: {platform.python_version()}")
 
 # --- IMAGEN DE PRESENTACIÓN ---
 try:
-    image = Image.open('postre.jpg')
+    image = Image.open('postre.jpeg')
     st.image(image, width=350)
 except Exception as e:
     st.warning(f"No se pudo cargar la imagen: {e}")
